13b9117c6d20353e831dd03913875f2fbde766fd
Phelipe de Sterlich <p.desterlich@gmail.com>
2017-02-15 01:17:21 +0100

    added basic api controller

---
+++ scopuli.common/Controllers/StarshipsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace scopuli.common.Controllers
+{
+    [Route("api/[controller]")]
+    public class StarshipsController : Controller
+    {
+        private readonly ScopuliDbContext _context;
+
+        public StarshipsController(ScopuliDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var starships = _context.Starships.ToList();
+
+            return Ok(starships);
+        }
+    }
+}
